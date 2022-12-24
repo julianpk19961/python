97c5edb963dc08f21986a6119636c541cb9a7451
Jcodfee <julianrodriguez19961@gmail.com>
2022-12-24 06:49:37 -0500

    changes

---
+++ CP-U/sesion3.py
@@ -0,0 +1,3 @@
+# Saltos de linea
+
+print('Elit officia magna nisi labore commodo cillum excepteur officia reprehenderit minim elit nostrud qui enim.\nLabore ea officia elit commodo.\nEu minim velit eu exercitation nulla occaecat ullamco eiusmod est laboris sint incididunt consequat.')
