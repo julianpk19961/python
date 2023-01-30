638de46da446859ccde883a9bf3de6240b71d073
Jcodfee <julianrodriguez19961@gmail.com>
2023-01-30 07:16:24 -0500

    Curso python x

---
+++ PythonX/variables.py
@@ -0,0 +1,31 @@
+# Variables in Python
+
+
+# How do create a variable and store some data in python?
+# Here are the steps we might need to follow.
+
+# Declare a lable to the memory location - Variable Name
+# Assign the value using 'Equals0 ('==') - Assignment
+# #Given the required value - Data needed to be stored
+
+# Defining a variable in python
+# Let's say, we have an application that needs to store the name of countries.
+# So here's how we will define a variable for it,
+
+country = 'Colombia'
+
+# Simple, ins't it?
+# Now let's say, we want to print the value that the variable 'country' holds                                                               [Holds = Sostiene/Contiene]
+# How to do that? We will just pass the variable name to the print() function, rather than passing a value is needed to be printed.         [rather = Mejor que/Mucho mEjor que]
+
+print(country)
+
+# Rules
+
+# Here are some set of rules that you might need to follow while declaring a variable
+#   A variable name should be meaningful and descriptive [Meaningful = Significativo/Útil/Valido]
+#   A variable name must start with a letter or the underscore character                                                                    [Underscore = Guión Bajo ]
+#   A variable name cannot start with a number
+#   A variable name can only contain alpha-mumeric characters and underscores (A...z,0...9,_)
+#   Variable names in Python are case-sensitive, this means country,Country,COUNTRY and CoUnTry are four different variables.
+#   A value, if it is a word or a characther, has to be encosled in double or single quotes.
